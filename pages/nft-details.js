3642c5f6ab811db24627cd519afa1c0211bfff22
dunkerbunker <cena26657@gmail.com>
2023-08-10 16:18:56 +0500

    a

--- pages/nft-details.js
+++ pages/nft-details.js
@@ -103,61 +103,65 @@ const NFTDetails = () => {
         </div>
       </div>
 
-      <div className="flex-1 justify-start sm:px-4 p-12 sm:pb-4">
-        <div className="flex flex-row sm:flex-col">
-          <h2 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl minlg:text-3xl">{nft.name}</h2>
-        </div>
-        <div className="mt-10">
-          <p className="font-poppins dark:text-white text-nft-black-1 text-xs minlg:text-base font-normal">Creator</p>
-          <div className="flex flex-row items-center mt-3">
-            <div className="relative w-12 h-12 minlg:w-20 minlg:h-20 mr-2">
-              <Image
-                src={images.creator1}
-                objectFit="cover"
-                className="rounded-full"
-              />
-            </div>
-            <p className="font-poppins dark:text-white text-nft-black-1 text-xs minlg:text-base font-semibold">
-              {shortenAddress(nft.seller)}
-            </p>
+      <div className="flex-1 justify-center sm:px-4 p-12 sm:pb-4">
+        <div className="flex flex-col items-center">
+          <h2 className="font-poppins dark:text-white text-nft-black-1 font-semibold text-6xl minlg:text-5xl text-center my-8">{nft.name}</h2>
+          <div className="flex flex-row my-4">
+            <button className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-xl font-medium border-b-2 border-transparent pb-1 focus:outline-none mr-8">
+              Details
+            </button>
+            <button className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-xl font-medium border-b-2 border-transparent pb-1 focus:outline-none">
+              History
+            </button>
+          </div>
+          <div className="w-full mt-6 text-center">
+            <p className="font-poppins dark:text-white text-nft-black-1 text-base font-normal p-4 text-justify">{nft.description}</p>
           </div>
         </div>
 
-        <div className="mt-10 flex flex-col">
-          <div className="w-full border-b dark:border-nft-black border-nft-gray-1 flex flex-row">
-            <p className="font-poppins dark:text-white text-nft-black-1 text-base minlg:text-base font-medium mb-2">
-              Details
-            </p>
-          </div>
-          <div className="mt-3">
-            <p className="font-poppins dark:text-white text-nft-black-1 text-base font-normal">
-              {nft.description}
-            </p>
-          </div>
-        </div>
-        <div className="flex flex-row sm:flex-col mt-10">
-          {currentAccount === nft.seller.toLowerCase() ? (
-            // if current account is the seller of the nft
-            <p className="font-poppins dark:text-white text-nft-black-1 text-base font-normal border border-gray p-2">
-              You can cannot buy your own NFT
-            </p>
-          ) : currentAccount === nft.owner.toLowerCase()
-            ? (
+        <div className="mt-10">
+          <p className="font-poppins dark:text-white text-nft-black-1 text-2xl minlg:text-base font-normal ml-4">Creator</p>
+          <div className="flex flex-row items-center justify-between mt-3 ml-4">
+            <div className="flex items-center">
+              <div className="relative w-24 h-24 minlg:w-40 minlg:h-40 mr-4">
+                <Image
+                  src={images.creator1}
+                  objectFit="cover"
+                  className="rounded-full"
+                />
+              </div>
+              <p className="font-poppins dark:text-white text-nft-black-1 text-xl minlg:text-lg font-semibold">
+                {shortenAddress(nft.seller)}
+              </p>
+            </div>
+
+            <div className="flex flex-row sm:flex-col justify-center">
+              {currentAccount === nft.seller.toLowerCase() ? (
+              // if current account is the seller of the nft
+                <p className="font-poppins dark:text-white text-nft-black-1 text-base font-normal border border-gray p-2">
+                  You cannot buy your own NFT
+                </p>
+              ) : currentAccount === nft.owner.toLowerCase() ? (
               // if current account is the owner of the nft
-              <Button
-                btnName="List on Marketplace"
-                classStyles="mr-5 sm:mr-0 sm:mb-5 rounded-xl"
-                handleClick={() => router.push(`/resell-nft?tokenId=${nft.tokenid}&tokenURI=${nft.tokenURI}`)}
-              />
-            ) : (
+                <Button
+                  btnName="List on Marketplace"
+                  classStyles="mr-5 sm:mr-0 sm:mb-5 rounded-xl"
+                  handleClick={() => router.push(`/resell-nft?tokenId=${nft.tokenid}&tokenURI=${nft.tokenURI}`)}
+                />
+              ) : (
               // if current account is a buyer of the nft
-              <Button
-                btnName={`Buy for ${nft.price} ${nftCurrency}`}
-                classStyles="mr-5 sm:mr-0 sm:mb-5 rounded-xl"
-                handleClick={() => setPaymentModal(true)}
-              />
-            )}
+                <button
+                  type="button"
+                  className="nft-gradient minlg:text-lg py-2 px-6 minlg:px-8 font-poppins font-semibold text-white mr-5 sm:mr-0 sm:mb-5 rounded-xl text-xl"
+                  onClick={() => setPaymentModal(true)}
+                >
+                  {`Buy for ${nft.price} ${nftCurrency}`}
+                </button>
+              )}
+            </div>
+          </div>
         </div>
+
       </div>
 
       {paymentModal && (
