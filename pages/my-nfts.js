22085b0b188b2505ad0096bcc80cbe5f9166f550
dunkerbunker <cena26657@gmail.com>
2023-08-14 22:55:53 +0500

    profile edit started

--- pages/my-nfts.js
+++ pages/my-nfts.js
@@ -1,8 +1,10 @@
 import { useState, useEffect, useContext } from 'react';
 import Image from 'next/image';
+import { useTheme } from 'next-themes';
 
+import { useDropzone } from 'react-dropzone';
 import { NFTContext } from '../context/NFTContext';
-import { NFTCard, Loader, Banner, SearchBar } from '../components';
+import { NFTCard, Loader, Banner, SearchBar, Modal, Button } from '../components';
 import images from '../assets';
 import { shortenAddress } from '../utils/shortenAddress';
 
@@ -12,6 +14,34 @@ const MyNFTs = () => {
   const [nftsCopy, setNftsCopy] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
   const [activeSelect, setActiveSelect] = useState('Recently Added');
+
+  const [isBannerModalOpen, setIsBannerModalOpen] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+  const { theme } = useTheme();
+
+  const handleBannerOpenModal = () => {
+    setIsBannerModalOpen(true);
+  };
+
+  const handleBannerCloseModal = () => {
+    setIsBannerModalOpen(false);
+  };
+
+  const onDrop = (acceptedFiles) => {
+    // Handle the dropped file (you can use the acceptedFiles array)
+    setSelectedFile(acceptedFiles[0]);
+  };
+
+  const { getRootProps, getInputProps, isDragActive, isDragAccept, isDragReject } = useDropzone({
+    onDrop,
+    accept: 'image/*',
+    maxSize: 10000000, // Increased the maxSize to 10MB
+  });
+
+  const fileStyle = `dark:bg-nft-black-1 bg-white border dark:border-white border-nft-gray-2 flex flex-col items-center p-5 rounded-sm border-dashed
+    ${isDragActive ? 'border-file-active ' : ''} 
+    ${isDragAccept ? 'border-file-accept ' : ''} 
+    ${isDragReject ? 'border-file-reject ' : ''}`;
 
   useEffect(() => {
     // fetch the nfts from the context
@@ -77,16 +107,39 @@ const MyNFTs = () => {
       <div className="w-full flexCenter flex-col">
         <Banner
           name="Your Nifty NFTs"
-          childStyles="text-center mb-4"
-          parentStyles="h-80 justify-center"
+          childStyles="text-center mb-4 relative"
+          parentStyles="h-80 justify-center relative"
         />
-
+        <div className="absolute top-10 right-2 p-2 pt-10 z-1">
+          <button
+            className="w-10 h-10 white-bg rounded-full flex items-center justify-center"
+            type="button"
+            onClick={handleBannerOpenModal}
+          >
+            {/* Edit icon for light mode */}
+            <Image
+              src={images.editLight}
+              width={28}
+              height={28}
+              objectFit="contain"
+              alt="Edit Icon"
+            />
+          </button>
+        </div>
         <div className="flexCenter flex-col -mt-20 z-0">
           <div className="flexCenter w-40 h-40 sm:w-36 sm:h-36 p-1 bg-nft-black-2 rounded-full">
             <Image
               src={images.creator1}
               className="rounded-full object-cover"
               objectFit="cover"
+            />
+          </div>
+          <div className="bottom-0 right-0 mb-1 mr-1">
+            <Image
+              src={images.editLight}
+              alt="Edit"
+              width={35}
+              height={35}
             />
           </div>
           <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-2xl mt-6">
@@ -118,6 +171,71 @@ const MyNFTs = () => {
             {nfts.map((nft) => <NFTCard key={nft.tokenId} nft={nft} onProfilePage />)}
           </div>
         </div>
+      )}
+
+      {isBannerModalOpen && (
+        <Modal
+          header="Update your banner!"
+          body={(
+            <div>
+              <div {...getRootProps()} className={fileStyle}>
+                <input {...getInputProps()} />
+                {selectedFile ? (
+                  <img
+                    src={URL.createObjectURL(selectedFile)}
+                    width={1000}
+                    height={300}
+                    alt="selected-file"
+                    className={theme === 'light' ? 'filter invert' : ''}
+                  />
+                ) : (
+                  <div className="flexCenter flex-col text-center">
+                    <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-lg">
+                      PNG, GIF, SVG, WEBM. Max 5MB.
+                    </p>
+                    <div className="my-12 w-full flex justify-center">
+                      <Image
+                        src={images.upload}
+                        width={100}
+                        height={100}
+                        objectFit="contain"
+                        alt="file-upload"
+                        className={theme === 'light' ? 'filter invert' : ''}
+                      />
+                    </div>
+                    <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm">
+                      Drag and Drop File
+                    </p>
+                    <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-sm mt-2">
+                      or Browser media on your device
+                    </p>
+                  </div>
+                )}
+              </div>
+              {/* Display the selected file */}
+              {selectedFile && (
+              <div className="p-4">
+                <p className="font-poppins dark:text-white text-nft-black-1 font-semibold text-lg">
+                  Selected File:  &nbsp;
+                  <span className="font-poppins dark:text-white text-nft-black-1 text-lg font-normal mt-2">
+                    {selectedFile.name}
+                  </span>
+                </p>
+              </div>
+              )}
+            </div>
+          )}
+          footer={selectedFile && (
+            <Button
+              btnName="Submit"
+              classStyles="mx-2 rounded-xl"
+              handleClick={() => {
+
+              }}
+            />
+          )}
+          handleClose={handleBannerCloseModal}
+        />
       )}
     </div>
   );
